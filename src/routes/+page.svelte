d9bb48a99cd60ed8873a690fdf5c5da5d3b7e850
Mirek Kędzierski <miroslaw.kedzierski@acoustic.co>
2023-11-08 20:41:33 +0100

    fix: update header menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Header from './Header.svelte'
+	import Header from './Header.svelte';
 	import Solutions from './Solutions.svelte';
 	import Tools from './Tools.svelte';
 	import Surveyors from './Surveyors.svelte';
--- src/routes/HeaderMenu.svelte
+++ src/routes/HeaderMenu.svelte
@@ -2,13 +2,23 @@
 
 <div class="relative flex items-center py-8">
 	<div class="logo">
-		<h1>Operantus</h1>
+		<img src="/Logo-BV.svg" alt="Beauro Veritas" />
 	</div>
 	<!-- nawifacja zamienić na tablicę obiektów -->
 	<nav class="ml-auto leading-6 bold font-semibold text-slate-700">
 		<ul class="flex space-x-8">
-			<li><a class="hover:text-sky-500 dark:hover:text-sky-400" href="/">Home</a></li>
-			<li>Wad</li>
+			<li>
+				<a
+					class="font-thin tracking-widest text-sm tracking-widest uppercase rounded-full flex items-center justify-center w-auto text-white"
+					href="/">Contact</a
+				>
+			</li>
+			<li>
+				<a
+					class="font-thin tracking-widest text-sm tracking-widest uppercase rounded-full flex items-center justify-center w-auto text-white"
+					href="/">ENG</a
+				>
+			</li>
 		</ul>
 	</nav>
 </div>
